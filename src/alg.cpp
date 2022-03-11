8522ad8735ca45b9f2176e838e98695747f3230c
JcKDanie1s <99392811+JcKDanie1s@users.noreply.github.com>
2022-03-11 13:06:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,7 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-    int i = 0, j = size - 1;
+   int i = 0, j = size - 1;
     while (i < j) {
         int mid = i + (j - i) / 2;
         if (arr[mid] == value) {
@@ -19,6 +19,7 @@ int cbinsearch(int *arr, int size, int value) {
             j = mid;
         } else {
             i = mid + 1;
+        }
     }
     return 0;
 }
